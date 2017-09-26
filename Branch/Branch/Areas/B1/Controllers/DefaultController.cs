f9b67bec748e4f074835a559821d840c96df58b1
bobtsui <grimmo58968@gmail.com>
2017-09-26 22:31:44 +0800

    a001-建立分支branch練習

---
+++ Branch/Branch/Areas/B1/Controllers/DefaultController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Branch.Areas.B1.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: B1/Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
